2b861560d6c9212a399f78fd0cef2d743b6879c9
zyr <zy19910126@126.com>
2021-11-08 13:41:19 +0800

    增加pb文件

--- components/network/example/echo/proto/init.go
+++ components/network/example/echo/proto/init.go
@@ -6,15 +6,6 @@ import (
 	"github.com/zylikedream/galaxy/components/network/message"
 )
 
-type EchoReq struct {
-	Msg string `json:"msg"`
-}
-
-type EchoAck struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-}
-
 func init() {
 	message.RegisterMessageMeta(int(crc32.ChecksumIEEE([]byte("EchoReq"))), (*EchoReq)(nil))
 	message.RegisterMessageMeta(int(crc32.ChecksumIEEE([]byte("EchoAck"))), (*EchoAck)(nil))
