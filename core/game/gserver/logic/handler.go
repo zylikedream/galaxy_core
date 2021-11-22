fc8051591f41595f7ba44e5091d0f50dbad46b27
zy-ubtu <zy-ubtu@zy-ubtu.com>
2021-11-22 21:10:30 +0800

    modify

--- core/game/gserver/logic/handler.go
+++ core/game/gserver/logic/handler.go
@@ -1,8 +1,13 @@
 package logic
 
 import (
+	"context"
+
+	"github.com/zylikedream/galaxy/core/game/gserver/module"
+	"github.com/zylikedream/galaxy/core/glog"
 	"github.com/zylikedream/galaxy/core/network/message"
 	"github.com/zylikedream/galaxy/core/network/session"
+	"go.uber.org/zap"
 )
 
 type LogicHandle struct {
@@ -20,5 +25,8 @@ func (l *LogicHandle) OnError(session.Session, error) {
 }
 
 func (l *LogicHandle) OnMessage(sess session.Session, m *message.Message) error {
+	if err := module.HandleMessage(context.Background(), m.Msg); err != nil {
+		glog.Error("handle message error", zap.Error(err))
+	}
 	return nil
 }
