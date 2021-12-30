3fccd4c34335354822046968b868e090dc4021b0
zyr <zy19910126@126.com>
2021-12-30 16:31:13 +0800

    remove game from core

--- core/game/gserver/src/component/accinfo.go
+++
@@ -1,8 +0,0 @@
-package component
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type RoleAccount struct {
-	RoleID  primitive.ObjectID `bson:"_id,omitempty"`
-	Account string             `bson:"account"`
-}
--- core/game/gserver/src/entity/entity.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-import "github.com/zylikedream/galaxy/core/game/gserver/src/gscontext"
-
-type Entity interface {
-	Load(ctx *gscontext.Context) error
-}
